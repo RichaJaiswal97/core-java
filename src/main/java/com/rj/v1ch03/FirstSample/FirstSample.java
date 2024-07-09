498b01206e3be3cf1ec9017cf1d086528971478c
rjrichiRich <jaiswalricha32@gmail.com>
2024-07-09 23:21:01 +0530

    v1ch03 | Adds FirstSample.java

--- src/main/java/com/rj/v1ch03/FirstSample/FirstSample.java
+++ src/main/java/com/rj/v1ch03/FirstSample/FirstSample.java
@@ -8,7 +8,6 @@ package com.rj.v1ch03.FirstSample;
  */
 public class FirstSample {
     public static void main(String[] args) {
-
         System.out.println("We will not use 'Hello, World!'");
     }
 }
