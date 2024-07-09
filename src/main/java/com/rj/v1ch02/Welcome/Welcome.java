614a3bddde64f2131e15ef3c3543e9c70a6634a0
rjrichiRich <jaiswalricha32@gmail.com>
2024-07-09 22:52:15 +0530

    Adds chapter 2 notes

---
+++ src/main/java/com/rj/v1ch02/Welcome/Welcome.java
@@ -0,0 +1,22 @@
+package com.rj.v1ch02.Welcome;
+
+/**
+ * This program displays a greeting for the reader.
+ * @version 1.30 2014-02-27
+ * @author Cay Horstmann
+ */
+public class Welcome
+{
+   /**
+    * This is a welcome function
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+      String greeting = "Welcome to Core Java!";
+      System.out.println(greeting);
+      for (int i = 0; i < greeting.length(); i++)
+         System.out.print("=");
+      System.out.println();
+   }
+}
